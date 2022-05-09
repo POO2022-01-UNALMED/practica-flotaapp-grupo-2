48fcdb23520ccaf85406c636aa1265b6a99d6f8b
Mateo Echavarria Sierra <63327224+TeoEchavarria@users.noreply.github.com>
2022-05-09 14:05:14 -0500

    Consultar y Agregar Saldo Usuario

--- FlotaApp-grupo2/src/terminal/Usuario.java
+++ FlotaApp-grupo2/src/terminal/Usuario.java
@@ -11,4 +11,16 @@ public class Usuario {
     private int cartera;
     private ArrayList<Usuario> historicoViajes;
 
+    public int consultarSaldo(){
+        return this.cartera;
+    }
+
+    public void agregarSaldo(int dinero){
+        if(dinero > 0){
+            this.cartera += dinero;
+        }else{
+            System.out.println("El dinero a agregar debe ser en numeros positivos");
+        }
+    }
+
 }
--- FlotaApp-grupo2/src/uiMain/Main.java
+++ FlotaApp-grupo2/src/uiMain/Main.java
@@ -8,11 +8,21 @@ public class Main {
         Usuario u1 = new Usuario();
         System.out.println(u1);
 
+        //Test Metodos Silla
         Silla s1 = new Silla();
-        System.out.println(s1.getEstado());
+        //System.out.println(s1.getEstado());
         s1.ocuparSilla();
-        System.out.println(s1.getEstado());
+        //System.out.println(s1.getEstado());
         s1.liberarSilla();
-        System.out.println(s1.getEstado());
+        //System.out.println(s1.getEstado());
+
+        //Test Metodos Usuario
+        System.out.println(u1.consultarSaldo());
+        u1.agregarSaldo(1000);
+        System.out.println(u1.consultarSaldo());
+        u1.agregarSaldo(-1000);
+        System.out.println(u1.consultarSaldo());
+
+
     }
 }
