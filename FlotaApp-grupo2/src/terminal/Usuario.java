30a10546575b716e34fa8fa723b6b4f20e054ec2
Mateo Echavarria Sierra <63327224+TeoEchavarria@users.noreply.github.com>
2022-05-09 18:53:29 -0500

    registrarse - eliminarUsuario Usuario

--- FlotaApp-grupo2/src/terminal/Usuario.java
+++ FlotaApp-grupo2/src/terminal/Usuario.java
@@ -1,5 +1,7 @@
 package terminal;
 
+import baseDatos.Serializador;
+
 import java.util.ArrayList;
 import java.io.Serializable;
 
@@ -11,11 +13,28 @@ public class Usuario implements Serializable {
     private int movil;
     private int cartera;
     private ArrayList<Viaje> historicoViajes;
-    private static ArrayList<Usuario> usuarios;
+    private static ArrayList<String> usuarios;
     static {
-        usuarios = new ArrayList<Usuario>();
+        usuarios = new ArrayList<String>();
     }
 
+    public void registrarse(int cc, String uNombre, String email, int movil){
+        this.cc = cc;
+        this.uNombre = uNombre;
+        this.email = email;
+        this.movil = movil;
+        this.cartera = 0;
+        this.historicoViajes = new ArrayList<>();
+        if (Usuario.getUsuarios().contains(this.toString()))
+        {
+            System.out.println("Este Usuario ya esta registrado");
+        }else{
+            Usuario.usuarios.add(this.toString());
+            Serializador.serializarTodo();
+        }
+
+
+    }
     @Override
     public String toString() {
         return "Usuario{" +
@@ -28,14 +47,10 @@ public class Usuario implements Serializable {
                 '}';
     }
 
-    public void registrarse(int cc, String uNombre, String email, int movil){
-        this.cc = cc;
-        this.uNombre = uNombre;
-        this.email = email;
-        this.movil = movil;
-        this.cartera = 0;
-        this.historicoViajes = new ArrayList<>();
-        Usuario.usuarios.add(this);
+    public static void eliminarUsuario(String usuario){
+        Usuario.usuarios.remove(usuario);
+        System.out.println(Usuario.getUsuarios());
+        Serializador.serializarTodo();
     }
 
     public int consultarSaldo(){
@@ -50,7 +65,7 @@ public class Usuario implements Serializable {
         }
     }
 
-    public static ArrayList<Usuario> getUsuarios(){
+    public static ArrayList<String> getUsuarios(){
         return usuarios;
     }
 
--- FlotaApp-grupo2/src/uiMain/Main.java
+++ FlotaApp-grupo2/src/uiMain/Main.java
@@ -9,9 +9,16 @@ public class Main {
     public static void main(String[] args) {
         Deserializador.deserializarTodo();
         Usuario u1 = new Usuario();
-        System.out.println(u1);
+        //System.out.println(u1);
         u1.registrarse(1, "Mateo E", "example@email.com", 1234567890);
-        System.out.println(u1);
+        //System.out.println(u1);
+
+        Usuario u2 = new Usuario();
+        //System.out.println(u2);
+        u2.registrarse(2, "Pablo", "example2@email.com", 2087654321);
+        //System.out.println(u2);
+        System.out.println(Usuario.getUsuarios());
+        Usuario.eliminarUsuario(u2.toString());
 
         //Test Metodos Silla
         Silla s1 = new Silla();
@@ -22,12 +29,12 @@ public class Main {
         //System.out.println(s1.getEstado());
 
         //Test Metodos Usuario
-        System.out.println(u1.consultarSaldo());
+        //System.out.println(u1.consultarSaldo());
         u1.agregarSaldo(1000);
-        System.out.println(u1.consultarSaldo());
+        //System.out.println(u1.consultarSaldo());
         u1.agregarSaldo(-1000);
-        System.out.println(u1.consultarSaldo());
+        //System.out.println(u1.consultarSaldo());
 
-        Serializador.serializarTodo();
+
     }
 }
