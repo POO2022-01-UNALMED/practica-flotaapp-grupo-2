9ce9902675a0c1dd40dd358c7a4ad5b3709a999d
jppinedal <54005283+jppinedal@users.noreply.github.com>
2022-05-14 12:13:19 -0500

    Create new branch

--- FlotaApp-grupo2/src/terminal/Viaje.java
+++ FlotaApp-grupo2/src/terminal/Viaje.java
@@ -1,12 +1,12 @@
 package terminal;
 import java.util.Date;
-
+//Se crea nueva rama 
 public class Viaje {
     private int id;
     private Ciudad origen;
     private Ciudad destino;
     private AdminTiquete tiquete;
     private Date fechaViaje;
-    private Vehiculo vehiculo;
+    private Vehiculo vehiculo; 
     private boolean escala;
 }
