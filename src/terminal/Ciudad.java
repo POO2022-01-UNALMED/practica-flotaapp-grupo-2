9d4322cab3fa1d126263f93d2315d77a4972006c
jppinedal <54005283+jppinedal@users.noreply.github.com>
2022-05-17 17:59:21 -0500

    Ciudad y viaje

--- src/terminal/Ciudad.java
+++ src/terminal/Ciudad.java
@@ -4,6 +4,33 @@ import java.util.ArrayList;
 
 public class Ciudad {
     private int id;
-    private ArrayList<String> nombre;
+    private String nombre; //
     private String dirTerminal;
+    private int numVisitantes;
+    private static  ArrayList<Ciudad> ciudades = new ArrayList<>();
+    
+    
+    public Ciudad(int id, String nombre, String dirTerminal, numVisitantes) { 
+    	this.id = id;
+    	this.nombre = nombre;
+    	this.numVisitantes = numVisitantes;
+    	this.numVisitantes = numVisitantes;
+    	ciudades.add(this); 
+    }
+    
+    public static void quitarCiudad(String ciudad) {
+    	if (!ciudades.isEmpty()){
+    		for (Ciudad c: ciudades) {
+        		if (c.nombre.equals(ciudad)) {
+        			ciudades.remove(c); //por que me saca error al intentar eliminar 
+        		}
+        	}
+    	}else {
+    		return;
+    	}
+
+    }
+    //historico de viajes 
+    
+    
 }
