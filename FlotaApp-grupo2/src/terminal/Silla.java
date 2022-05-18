b61db0280cbdf52d55860d0c69f89134c30030ed
haison12 <36688293+haison12@users.noreply.github.com>
2022-05-18 18:05:47 -0500

    Clase silla
    En esta clase agregué el enum y el método get para el atributo estado.

--- FlotaApp-grupo2/src/terminal/Silla.java
+++ FlotaApp-grupo2/src/terminal/Silla.java
@@ -1,11 +1,22 @@
 package terminal;
 
+import java.util.ArrayList;
+
 public class Silla {
+	private enum ubicacion {PASILLO, VENTANA, INTERMEDIO}
 
     private int numeroSilla;
     private boolean tipo; //0 - Estandar , 1 - Booleana
     private boolean ubicacion; //0 - V , 1 - P
     private boolean estado;
+    
+    public Silla(int numeroSilla, boolean tipo, boolean ubicacion, boolean estado) {
+    	this.numeroSilla = numeroSilla;
+    	this.tipo = tipo;
+    	this.ubicacion = ubicacion;
+    	this.estado = estado;
+    	
+    }
 
     public Silla ocuparSilla(){
         this.estado = true;
@@ -20,4 +31,10 @@ public class Silla {
     public boolean getEstado() {
         return estado;
     }
+    
+    public void setEstado(boolean estado) {
+    	this.estado = estado;
+    }
+    
+    
 }
