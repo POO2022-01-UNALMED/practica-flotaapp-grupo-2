c8a2f81d3a776b594ecfcf85b1338be403e8d4cd
haison12 <36688293+haison12@users.noreply.github.com>
2022-05-18 18:07:25 -0500

    Create Empleado.java

---
+++ src/terminal/Empleado.java
@@ -0,0 +1,13 @@
+
+package terminal;
+
+public class Empleado extends Usuario {
+	
+	public Empleado(int cc, String uNombre, String email, long movil) {
+		super(cc, uNombre , email, movil);
+	}
+	
+	public void renuciar() {}
+	
+	public void despedir() {}
+}
