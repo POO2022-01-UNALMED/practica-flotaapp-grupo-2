df4fd6f70ca69f02b064014b81d16250fcd944a2
Mateo Echavarria Sierra <63327224+TeoEchavarria@users.noreply.github.com>
2022-05-30 09:28:10 -0500

    Actualizacion de Serializador - Deserializador

--- src/uiMain/Main.java
+++ src/uiMain/Main.java
@@ -12,7 +12,7 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Deserializador.deserializarTodo();
-
+        /*
         // COMPRADORES
         Comprador u1 = new Comprador(1, "Usuario1", "example@email.com", 3234567890L);
         Comprador u2 = new Comprador(2, "Usuario2", "example2@email.com", 3087654321L);
@@ -22,23 +22,23 @@ public class Main {
 
         ////FECHAS PRUEBAS
 
-        Date fin = new Date(2022,1,1);
-        Date inicio = new Date(2020,3,12);
-        Date intermedio = new Date(2020,7,14);
-        Date fechaCompra = new Date(2020,6,22);
+        Date fin = new Date(2022, 1, 1);
+        Date inicio = new Date(2020, 3, 12);
+        Date intermedio = new Date(2020, 7, 14);
+        Date fechaCompra = new Date(2020, 6, 22);
 
         ////CIUDADES
 
-        Ciudad c1 = new Ciudad(1,"Medellin", "calle X - 95");
-        Ciudad c2 = new Ciudad(2,"Bello", "calle Y - 72");
-        Ciudad c3 = new Ciudad(7,"Popayan", "calle X - 37");
-        Ciudad c4 = new Ciudad(8,"Cali", "calle F - 13");
+        Ciudad c1 = new Ciudad(1, "Medellin", "calle X - 95");
+        Ciudad c2 = new Ciudad(2, "Bello", "calle Y - 72");
+        Ciudad c3 = new Ciudad(7, "Popayan", "calle X - 37");
+        Ciudad c4 = new Ciudad(8, "Cali", "calle F - 13");
 
         //////SILLAS
-        Silla sp1 = new Silla(1, true , Ubicacion.VENTANA);
-        Silla sp2 = new Silla(2, true , Ubicacion.PASILLO);
-        Silla se1 = new Silla(7, false , Ubicacion.VENTANA);
-        Silla se2 = new Silla(8, false , Ubicacion.PASILLO);
+        Silla sp1 = new Silla(1, true, Ubicacion.VENTANA);
+        Silla sp2 = new Silla(2, true, Ubicacion.PASILLO);
+        Silla se1 = new Silla(7, false, Ubicacion.VENTANA);
+        Silla se2 = new Silla(8, false, Ubicacion.PASILLO);
         ArrayList<Silla> sillas = new ArrayList<Silla>();
         sillas.add(sp1);
         sillas.add(sp2);
@@ -49,15 +49,15 @@ public class Main {
         Vehiculo v1 = new Vehiculo("AAA000", sillas);
 
         //VIAJES
-        Viaje viaje1 = new Viaje(12,300000, 4000, 7000, c2,c4,7, v1 ,intermedio);
+        Viaje viaje1 = new Viaje(12, 300000, 4000, 7000, c2, c4, 7, v1, intermedio);
 
 
         //EMPLEADOS
 
         ////MECANICOS
-        Especialista mec1 = new Especialista(27, "Jose", "emailMecanico1@example.com", 3224568585L, 3000,Especialidad.MECANICO);
-        Especialista mec2 = new Especialista(28, "Maria", "emailMecanico2@example.com", 3224567585L, 4000,Especialidad.ELECTRICO);
-        Especialista mec3 = new Especialista(28, "Pablo", "emailMecanico3@example.com", 3224538585L, 3700,Especialidad.MECANICO);
+        Especialista mec1 = new Especialista(27, "Jose", "emailMecanico1@example.com", 3224568585L, 3000, Especialidad.MECANICO);
+        Especialista mec2 = new Especialista(28, "Maria", "emailMecanico2@example.com", 3224567585L, 4000, Especialidad.ELECTRICO);
+        Especialista mec3 = new Especialista(28, "Pablo", "emailMecanico3@example.com", 3224538585L, 3700, Especialidad.MECANICO);
 
         ////CONDUCTORES
         Conductor con1 = new Conductor(28, "Don Javie", "DonJavier@example.com", 3004569696L, 4000, Categoria.B3);
@@ -73,9 +73,9 @@ public class Main {
         System.out.println(u2.historicoViaje(inicio, fin));
 
         //////funcionamiento de comprarTiquete
-        System.out.println("u1: compra el tiquete: "+ u1.comprarTiquete(c2, c4, 4000));
-        System.out.println("u3: compra el tiquete: "+u2.comprarTiquete(c2, c4, 5000));
-        System.out.println("u3: compra el tiquete: "+u3.comprarTiquete(c2, c4, 10000));
+        System.out.println("u1: compra el tiquete: " + u1.comprarTiquete(c2, c4, 4000));
+        System.out.println("u3: compra el tiquete: " + u2.comprarTiquete(c2, c4, 5000));
+        System.out.println("u3: compra el tiquete: " + u3.comprarTiquete(c2, c4, 10000));
 
         System.out.println(u2.getHistoricoViajes());
 
@@ -88,9 +88,11 @@ public class Main {
         System.out.println(Asignar.asignarVehiculo(mec1, v1));
 
         System.out.println(Comprador.getCompradores());
+        Serializador.serializarTodo(); */
+
 
         AdminViaje.visualizarEstadisticas();
         Gestionar.gestionarEspecialistas();
-        Serializador.serializarTodo();
+
     }
 }
