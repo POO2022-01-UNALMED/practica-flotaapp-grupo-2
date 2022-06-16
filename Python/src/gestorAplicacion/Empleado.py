6908f6a93973508807ce0420b5320891234de7c0
Mifonsecaa <92172868+Mifonsecaa@users.noreply.github.com>
2022-06-16 09:57:57 -0500

    Modificación clase Empleado
    Puse Especialidad dentro de Empleado para saber si el empleado es Admin.

--- Python/src/gestorAplicacion/Empleado.py
+++ Python/src/gestorAplicacion/Empleado.py
@@ -1,4 +1,15 @@
 from gestorAplicacion.Usuario import Usuario
+from enum import Enum
+
+class Especialidad(Enum):
+
+    ELECTRICO = "ELECTRICO"
+
+    MECANICO = "MECANICO"
+
+    SILLETERIA = "SILLETERIA"
+
+    ADMINISTRADOR = "ADMINISTRADOR"
  #Superclase Empleado
 
 class Empleado(Usuario):
@@ -11,3 +22,6 @@ class Empleado(Usuario):
         
     def bonoSueldo(self):
         self._billetera += self._billetera * 0.1
+
+    def getEspecialidad(self):
+        return Especialidad
