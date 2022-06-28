8b60418bd833efdca8d9791ec18cd970ad603352
Mateo Echavarria Sierra <63327224+TeoEchavarria@users.noreply.github.com>
2022-06-28 13:42:05 -0500

    Update Recomendacion.py

--- Python/src/uiMain/Funcionalidades/Recomendacion.py
+++ Python/src/uiMain/Funcionalidades/Recomendacion.py
@@ -1,6 +1,7 @@
 """
 Recomendacion de viajes a Usuarios
 """
+<<<<<<< Updated upstream
 from gestorAplicacion.viajes.Ciudad import Ciudad
 from gestorAplicacion.personas.Comprador import Comprador
 from gestorAplicacion.personas.Usuario import Usuario
@@ -49,3 +50,59 @@ class Recomendacion():
                 return print(f"Te recomendamos viajar a: {masVisitada.__str__()} \n")
 
         print("ID DE USUARIO NO ENCONTRADO")
+=======
+from gestorAplicacion.Ciudad import Ciudad
+from gestorAplicacion.Comprador import Comprador
+from gestorAplicacion.Usuario import Usuario
+from gestorAplicacion.Viaje import Viaje
+from gestorAplicacion.Tiquete import Tiquete
+
+
+
+class Recomendacion():
+    __visitadas = {}
+    
+    
+    @staticmethod()
+    def recomendarViaje(cls, cc: int = 0):
+        print("----- R E C O M E N D A R   V I A J E -----" + "\n")
+        recomendadisima = Ciudad()
+        aRecomendar = Comprador()
+        for comprador in Comprador.getComprador():
+            if comprador.getCC == cc:
+                aRecomendar = comprador
+                
+            if (aRecomendar in Comprador().getComprador()) and (len(aRecomendar.getHistocioViaje()) > 0 and aRecomendar != None):
+                for cadaTiquete in aRecomendar.getHistocioViaje():
+                    if not Recomendacion().__visitadas:
+                        Recomendacion.__visitadas.setdefault(cadaTiquete().getViaje().getDestino(), 1)
+                    elif cadaTiquete().getViaje().getDestino() in Recomendacion().__visitadas:
+                        Recomendacion().__visitadas[cadaTiquete().getViaje().getDestino()] = Recomendacion().__visitadas.get(Recomendacion().__visitadas[cadaTiquete().getViaje().getDestino()] + 1) #Agrega 1 a la ciudad
+                    else:
+                        Recomendacion.__visitadas.setdefault(cadaTiquete().getViaje().getDestino(), 1)
+                masVisitada = None
+                visitas = 0
+            
+                for cadaCiudad in Recomendacion().__visitadas.keys():
+                    if Recomendacion().__visitadas.get(cadaCiudad) > visitas:
+                        masVisitada = cadaCiudad
+                        visitas = Recomendacion().__visitadas.get(cadaCiudad)
+            
+                recomendadisima = masVisitada
+            else:
+                masVisitada: Ciudad = None
+                visitas = 0
+                
+                for cadaCiudad in Ciudad().getCiudades():
+                    if cadaCiudad.getNumVisitantes() > visitas:
+                        masVisitada = cadaCiudad
+                        visitas = cadaCiudad.getNumVisitantes()
+                recomendadisima = masVisitada
+            print("Te recomendamos viajar a: " + recomendadisima.__str__())
+                        
+              
+                        
+                
+        
+        
+>>>>>>> Stashed changes
